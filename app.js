f6bb817b412dc31d53892dfb86a6e17fb164cfc3
palniraj <palniraj21@gmail.com>
2023-12-17 06:02:12 +0545

    mongodb config

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
-// import dependances 
+// import dependances
 // always express-async-errors on top
 require("express-async-errors");
 const express = require("express");
 const errorHandler = require("./handlers/errorHandler");
-
+const mongoose = require("mongoose");
 require("dotenv").config();
 
 // create a server
 const app = express();
+mongoose
+  .connect(process.env.mongo_connection, {})
+  .then(() => {
+    console.log("Mongo connection successfull!");
+  })
+  .catch(() => {
+    console.log("Mongo connection failed!");
+  });
 
 app.use(express.json());
 
 // end of all routes import errorhandler
 app.use(errorHandler);
-app.listen(8000, ()=>{
-    console.log("Server started successfully!");
+app.listen(8000, () => {
+  console.log("Server started successfully!");
 });
