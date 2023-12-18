44bc3baae22c2a43c1590c4e6bdf7226d5248ad3
palniraj <palniraj21@gmail.com>
2023-12-19 03:20:56 +0545

    test userDashboard without auth

---
+++ modules/users/controllers/userDashboard.js
@@ -0,0 +1,6 @@
+const userDashboard = (req, res) =>{
+    res.status(200).json({
+        status: "Hello from userRegister!"
+    });
+}
+module.exports = userDashboard;
--- modules/users/users.routes.js
+++ modules/users/users.routes.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const register = require("./controllers/register");
 const login = require("./controllers/login");
+const userDashboard = require("./controllers/userDashboard");
 
 const userRoutes = express.Router();
 
 // Routes...
 userRoutes.post("/register", register);
 userRoutes.post("/login", login);
+userRoutes.get("/dashboard", userDashboard);
 
 module.exports = userRoutes;
