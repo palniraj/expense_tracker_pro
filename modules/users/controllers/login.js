2e954146d2921aa4ca1f2a8a3a73ea208f6aa064
palniraj <palniraj21@gmail.com>
2023-12-19 03:43:03 +0545

    jwt uses

--- modules/users/controllers/login.js
+++ modules/users/controllers/login.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
-
+const jsonwebtoken = require("jsonwebtoken");
 const login = async(req, res) =>{
 
     const usersModel = mongoose.model("users");
@@ -14,9 +14,17 @@ const login = async(req, res) =>{
     const comparePassword = await bcrypt.compare(password, getUser.password);
     if(!comparePassword) throw "Email and password do not match!";
 
+    const accessToken = await jsonwebtoken.sign({
+    _id: getUser._id, 
+    name: getUser.name,
+}, 
+    process.env.jwt_salt
+    );
+
     res.status(200).json({
         status: "success",
-        message: "User logged in successfully!"
+        message: "User logged in successfully!",
+        accessToken: accessToken,
     });
 }
 module.exports = login;
