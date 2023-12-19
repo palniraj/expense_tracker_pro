a983e5a1c7053389c78e25c9aba171271843bbc6
palniraj <palniraj21@gmail.com>
2023-12-19 07:56:07 +0545

    add income route

--- modules/transactions/controllers/addincome.js
+++ modules/transactions/controllers/addincome.js
@@ -1,17 +1,42 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
-const addIncome = (req, res) =>{
-    const usersModel = mongoose.model("users");
-    const transactionsModel = mongoose.model("transactions");
-    
-    const {amount, remarks} = req.body;
-    if(!amount) throw "Amount is required!";
-    if(!remarks) throw "Remarks is required!";
-    if(remarks.length < 5) throw "Remarks must be at least 5 characters long!";
+const addIncome = async (req, res) => {
+  const usersModel = mongoose.model("users");
+  const transactionsModel = mongoose.model("transactions");
 
-    res.status(200).json({
+  const { amount, remarks } = req.body;
+  if (!amount) throw "Amount is required!";
+  if (!remarks) throw "Remarks is required!";
+  if (remarks.length < 5) throw "Remarks must be at least 5 characters long!";
+  // console.log(validator.isNumeric(amount.toString()));
+  if (!validator.isNumeric(amount.toString()))
+    throw "Amount must be a valid number!";
+  //work with database
+  await transactionsModel.create({
+    user_id: req.user._id,
+    amount: amount,
+    remarks: remarks,
+    transaction_type: "income",
+  });
+  //   update user  balance
+  await usersModel.updateOne(
+    {
+      _id: req.user._id,
+    },
+    {
+      $inc: {
+        balance: amount,
+      },
+    },
+    {
+      runValidators: true,
+    }
+  );
+
+  res.status(200).json({
     status: "success",
     message: "Income added successfully!",
-    });
+  });
 };
 module.exports = addIncome;
