d6944af7546280aec81e8fc57434033154b7ff00
palniraj <palniraj21@gmail.com>
2023-12-29 02:12:53 +0545

    forgot password routed

--- modules/users/users.routes.js
+++ modules/users/users.routes.js
@@ -3,12 +3,14 @@ const register = require("./controllers/register");
 const login = require("./controllers/login");
 const userDashboard = require("./controllers/userDashboard");
 const auth = require("../../middleware/auth");
+const forgotPassword = require("./forgotPassword");
 
 const userRoutes = express.Router();
 
 // Routes...
 userRoutes.post("/register", register);
 userRoutes.post("/login", login);
+userRoutes.post("/forgotpw", forgotPassword);
 // Middleware...
 userRoutes.use(auth);
 // protected routes...
