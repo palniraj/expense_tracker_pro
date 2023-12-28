b98091ec8d3899bb1a1581ad04e958833967490e
palniraj <palniraj21@gmail.com>
2023-12-29 01:34:25 +0545

    get transactions_type income or expese data only

--- modules/transactions/controllers/getTransactions.js
+++ modules/transactions/controllers/getTransactions.js
@@ -3,8 +3,13 @@ const mongoose = require("mongoose");
 const getTransactions = async (req, res) =>{
     
     const transactionsModel = mongoose.model("transactions");
+    //query string parameter for finding spection transactions like expense only, income only
+    // console.log(req.query);
+
     const transactions = await transactionsModel.find({
         user_id: req.user._id,
+        // ...spread operator -> transaction_type: 'income'
+        ...req.query,
     });
     res.status(200).json({
         status: "success",
