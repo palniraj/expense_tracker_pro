3540f7e05542902704f8ca5c8a34aaf369fcffbe
palniraj <palniraj21@gmail.com>
2023-12-29 02:22:36 +0545

    sending reset code on email

--- modules/users/forgotPassword.js
+++ modules/users/forgotPassword.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const nodemailer = require("nodemailer");
 
 const forgotPassword = async (req, res) => {
   // importing users Model
@@ -9,7 +10,37 @@ const forgotPassword = async (req, res) => {
     email: email,
   });
   if (!getUser) throw "This email doesnot exit in the system!";
+  // 5 random digit number
+  const resetCode = Math.floor(10000 + Math.random() * 90000);
 
+  await usersModel.updateOne(
+    {
+      email: email,
+    },
+    {
+      reset_code: resetCode,
+    },
+    {
+      runValidators: true,
+    }
+  );
+  // configuration for nodemailer (trasporter)
+  var transport = nodemailer.createTransport({
+    host: "sandbox.smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "ed3f281a1566dc",
+      pass: "5f9692aed81837",
+    },
+  });
+  // use it for sending email
+  transport.sendMail({
+    to: email,
+    from: "info@expensetracker.com",
+    text: "Your password rest code is " + resetCode,
+    html: "Your password rest code is " + resetCode,
+    subject: "Reset your password - Expense Tracker Pro!",
+  });
   res.status(200).json({
     status: "success",
   });
