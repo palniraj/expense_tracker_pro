e701d1b1e8f6ea47ecbcd7a981b28e28db4f2b5c
palniraj <palniraj21@gmail.com>
2023-12-29 01:26:07 +0545

    get all transactions

--- modules/transactions/transactions.routes.js
+++ modules/transactions/transactions.routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const auth = require("../../middleware/auth");
 const addIncome = require("./controllers/addincome");
 const addExpense = require("./controllers/addExpense");
+const getTransactions = require("./controllers/getTransactions");
 
 const transactionRoutes = express.Router();
 
@@ -10,5 +11,6 @@ transactionRoutes.use(auth);
 // protected routes...
 transactionRoutes.post("/addIncome", addIncome);
 transactionRoutes.post("/addExpense", addExpense);
+transactionRoutes.get("/", getTransactions);
 
 module.exports = transactionRoutes;
