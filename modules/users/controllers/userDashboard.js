d89fa505290fc98d45fe4e23689e1b58c69e0dc5
palniraj <palniraj21@gmail.com>
2023-12-29 01:44:06 +0545

    finalizing user dashboard with geting transactions

--- modules/users/controllers/userDashboard.js
+++ modules/users/controllers/userDashboard.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const userDashboard = async (req, res) => {
   const usersModel = mongoose.model("users");
+  const transactionsModel = mongoose.model("transactions");
   console.log(req.user);
   const getUser = await usersModel
     .findOne({
@@ -9,9 +10,16 @@ const userDashboard = async (req, res) => {
     })
     .select("-password");
   // .select("name balace email");
+
+  const transactions = await transactionsModel.find({
+    user_id: req.user._id,
+  }).sort("-createdAt").limit(5);
+
   res.status(200).json({
     status: "success",
     data: getUser,
+    transactions, 
+    // same transactions: trandactions
   });
 };
 module.exports = userDashboard;
